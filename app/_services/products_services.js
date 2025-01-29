c223afe07be4cd1f015f946867a015764c4b6d47
Alice <iv.alisa21@gmail.com>
2025-01-29 08:49:33 -0700

    adds functions for edit and delete product by id under user

---
+++ app/_services/products_services.js
@@ -0,0 +1,35 @@
+import { collection, getDoc, updateDoc } from "firebase/firestore";
+
+export async function dbDeleteProductById(userId, productId, productSetter){
+    try {
+        const productRef = collection(db, 'users', userId, 'products', productId)
+        const documentSnapshot = await getDoc(productRef);
+        
+        if (documentSnapshot.exists()) {
+            const productData = documentSnapshot.data();
+            return productSetter(productData);
+          } else {
+            console.log("The product in the database fdoes not exist under user");
+          }
+
+    } catch (error) {
+        console.error("Error fetching random lesson:", error);
+    }
+}
+
+export async function dbEditProductById(userId, productId, newProductData) {
+  try {
+      const productRef = doc(db, 'users', userId, 'products', productId);
+      
+      const documentSnapshot = await getDoc(productRef);
+
+      if (documentSnapshot.exists()) {
+          await updateDoc(productRef, newProductData);
+          console.log("Product updated successfully.", productId);
+      } else {
+          console.log("The product does not exist in the database under user.", userId);
+      }
+  } catch (error) {
+      console.error("Error updating product:", error);
+  }
+}

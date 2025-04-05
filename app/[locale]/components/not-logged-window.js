10d9dbe4dfc41a8a3009cfb691b0d1ddbad3551a
Thong Nguyen <giathong3012005@gmail.com>
2025-04-04 23:58:58 -0600

    update not logged window and sign in

--- app/[locale]/components/not-logged-window.js
+++ app/[locale]/components/not-logged-window.js
@@ -1,26 +1,27 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function NotLoggedWindow() {
+  const t = useTranslations("notLogged");
+
   return (
     <div className="inset-0 flex items-center justify-center bottom-44">
       <div className="w-96 p-6 flex flex-col text-left items-center gap-4">
         <p className="text-lg text-left">
-          Welcome to <span className="font-bold">Artisan Track</span>!  
-          Manage your products, orders, materials and finance.  
+          {t("welcome")} <span className="font-bold">Artisan Track!</span>
+          {t("description")}
         </p>
-        <p className="text-left text-gray-600">
-          Sign in to continue or create a new account to get started.
-        </p>
+        <p className="text-left text-gray-600">{t("prompt")}</p>
 
         <div className="flex flex-col gap-3 w-full">
           <Link href="/login">
             <button className="font-bold bg-green py-2 px-4 rounded-lg w-full">
-              Log In
+              {t("login")}
             </button>
           </Link>
           <Link href="/signin">
             <button className="font-bold border-2 border-green py-2 px-4 rounded-lg w-full">
-              Create Account
+              {t("createAccount")}
             </button>
           </Link>
         </div>

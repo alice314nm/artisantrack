fc7db2d81cbd3c746474f26d178ffc67e0bc44b7
Alice <iv.alisa21@gmail.com>
2025-04-05 23:20:01 -0600

    fox 0.1

--- middleware.js
+++ middleware.js
@@ -1,17 +1,11 @@
 // middleware.js
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
 
-export default createMiddleware({
-  ...routing,
-  localeDetection: true, // Important: tries to detect the locale
-  // Prefer locale in cookie if present
-  getLocale: ({ request }) => {
-    const locale = request.cookies.get('NEXT_LOCALE')?.value;
-    return locale;
-  }
-});
+export default createMiddleware(routing);
 
 export const config = {
+  // Skip all paths that should not be internationalized.
+  // This skips the root route and all API routes
   matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
 };

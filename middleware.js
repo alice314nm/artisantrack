492b37d79f45bc4b253ef367e9d12223bc25d67b
Alice <iv.alisa21@gmail.com>
2025-04-05 23:05:41 -0600

    default next-intl

--- middleware.js
+++ middleware.js
@@ -2,14 +2,10 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware({
-  // locales and defaultLocale must match your routing setup
-  locales: routing.locales,
-  defaultLocale: routing.defaultLocale,
-});
+export default createMiddleware(routing);
+
 export const config = {
   // Skip all paths that should not be internationalized.
   // This skips the root route and all API routes
   matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
 };
-
